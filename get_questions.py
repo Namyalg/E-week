8ea657038f672a54f7c9d50d4c9caa8d765c63a1
namrun <nowitsharder@gmail.com>
2020-10-05 01:49:57 +0530

    Added python script to fetch data from firebase and store as a csv file

--- get_questions.py
+++ get_questions.py
@@ -1,14 +1,37 @@
-from firebase import firebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import csv
+
+fields = ["Timestamp" , "Question"]
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('firebase-adminsdk.json')
+cred = credentials.Certificate('e-week-firebase-adminsdk-uvnma-2d671aae5c.json')
+
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://e-week.firebaseio.com/'
+        'databaseURL': 'https://e-week.firebaseio.com/'
 })
 
 ref = db.reference('Questions')
-print(ref.get())
+
+filename = "questions.csv"
+    
+    # writing to csv file  
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object  
+    csvwriter = csv.writer(csvfile)  
+    # writing the fields  
+    csvwriter.writerow(fields)  
+
+
+for i,j in ref.get().items():
+    try:
+        rows = [i, j]
+        with open(filename, "a") as csvfile:
+            csvwriter = csv.writer(csvfile)  
+            csvwriter.writerows(rows) 
+    except:
+        pass
+    
+    print(i, j)
